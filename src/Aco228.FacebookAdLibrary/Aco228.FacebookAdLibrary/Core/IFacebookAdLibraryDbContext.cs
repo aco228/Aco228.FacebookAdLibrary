de83be2a22de11ae38d8290a28966e396d67decb
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-03-01 18:35:28 +0100

    Initial commit

---
+++ src/Aco228.FacebookAdLibrary/Aco228.FacebookAdLibrary/Core/IFacebookAdLibraryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Aco228.MongoDb.Services;
+
+namespace Aco228.FacebookAdLibrary.Core;
+
+public interface IFacebookAdLibraryDbContext : IMongoDbContext
+{
+    
+}
+
+public class FacebookAdLibraryDbContext : MongoDbContext, IFacebookAdLibraryDbContext
+{
+    public override string DatabaseName => "FacebookAdLibrary";
+    protected override string GetConnectionString()
+        => GetConnectionStringFromEnv("MONGO_CONNECTION_STRING");
+}
---
+++ src/Aco228.FacebookAdLibrary/Aco228.FacebookAdLibrary/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Aco228.Common.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aco228.FacebookAdLibrary;
+
+public static class ServiceExtensions
+{
+    public static void RegisterFacebookAdLibraryServices(this IServiceCollection services)
+        => typeof(ServiceExtensions).RegisterIfNot(() =>
+        {
+
+        });
+}
